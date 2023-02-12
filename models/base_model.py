ceb600e5e2af95f262be32c7063c28935dd30934
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2023-02-12 23:26:29 +0200

    final console

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,46 @@
 #!/usr/bin/python3
+"""Defines the BaseModel class."""
 import uuid
 import datetime
-
+import models
 
 class BaseModel():
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
         self.id = uuid.uuid4()
         self.created_at = datetime.datetime().now()
         self.updated_at = datetime.datetime().now()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
     def __str__(self):
-        return __class__ + self.id +self.__dict__
+        """Return the print/str representation of the BaseModel instance."""
+        return self.__class__.__name__ + self.id +self.__dict__
     
     def save(self):
-        self.updated_at = datetime.datetime().now().isoformat("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = datetime.datetime().now()
+        models.storage.save()
 
     def to_dict(self):
-        return self.__dict__
+        """Return the dictionary of the BaseModel instance.
+
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
